916ab0ca1433cd9f0356313266acb1465659e4d3
Andres <andres.gonzalez2392@gmail.com>
2022-07-29 10:53:47 -0400

    Template full responsive
    Template full responsive

--- js/app.js
+++ js/app.js
@@ -4,8 +4,28 @@ const skills_bars = document.querySelectorAll(".skill-progress");
 const records_wrap = document.querySelector(".records");
 const records_numbers = document.querySelectorAll(".number");
 const footer_input = document.querySelector(".footer-input");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 
+//--------SMALL MENU ------------//
+function closeMenu() {
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+  }
+  
+  hamburger_menu.addEventListener("click", () => {
+    if (!navbar.classList.contains("open")) {
+      navbar.classList.add("open");
+      document.body.classList.add("stop-scrolling");
+    } else {
+      closeMenu();
+    }
+  });
+  
+  links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+  
 //------FILTER PORTFOLIO ---------//
 footer_input.addEventListener("focus", () => {
     footer_input.classList.add("focus");
